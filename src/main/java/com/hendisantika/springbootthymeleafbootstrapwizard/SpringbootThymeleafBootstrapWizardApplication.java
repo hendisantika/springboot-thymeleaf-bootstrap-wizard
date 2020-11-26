0613560f96168966f308f3677716095aa42119d1
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-27 05:53:52 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootthymeleafbootstrapwizard/SpringbootThymeleafBootstrapWizardApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafbootstrapwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafBootstrapWizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThymeleafBootstrapWizardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootthymeleafbootstrapwizard/SpringbootThymeleafBootstrapWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafbootstrapwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafBootstrapWizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
